4eb47e10a7cafd23f811290b94343a126f37c99a
aidenjanzen <aidenw.janzen@outlook.com>
2024-05-08 11:30:09 -0700

    Code cleanup

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,1 @@
-from routes.api_customers import api_customers_bp
-from routes.api_products import api_products_bp
-from routes.api_orders import api_orders_bp
 from routes.endpoints import web_pages_bp
-
-from routes.api_final import api_final_bp
--- routes/endpoints.py
+++ routes/endpoints.py
@@ -28,61 +28,6 @@ def gen(number):
     startNumber = [1, 152, 252, 387, 494, 650, 722, 810, 906]
     return render_template("generations.html", number=number, pokedex=pokedex, amounts=amounts, startNumber=startNumber)
 
-@web_pages_bp.route("/customers")
-def customers():
-    statement = db.select(Customer).order_by(Customer.name)
-    records = db.session.execute(statement)
-    results = records.scalars()
-    return render_template("customers.html", customers=results)
-
-
-@web_pages_bp.route("/customers/<int:customer_id>")
-def customer_details(customer_id):
-    statement = db.select(Customer).where(Customer.id == customer_id)
-    result = db.session.execute(statement)
-    customer = result.scalar()
-    return render_template("customerorders.html", customer=customer)
-
-
-@web_pages_bp.route("/products")
-def products():
-    statement = db.select(Product).order_by(Product.name)
-    records = db.session.execute(statement)
-    results = records.scalars()
-    return render_template("products.html", products=results)
-
-
-@web_pages_bp.route("/orders")
-def orders():
-    statement = db.select(Order).order_by(Order.id)
-    records = db.session.execute(statement)
-    results = records.scalars()
-    return render_template("orders.html", orders=results,) #customers=results1
-
-@web_pages_bp.route("/orders/<int:order_id>")
-def order_details(order_id):
-    statement = db.select(Order).where(Order.id == order_id)
-    result = db.session.execute(statement)
-    order = result.scalar()
-    return render_template("orderdetails.html", order=order)
-
-@web_pages_bp.route("/orders/<int:order_id>/delete", methods=["POST"])
-def order_delete(order_id):
-    order = db.get_or_404(Order, order_id)
-    statement= db.select(ProductOrder).where(ProductOrder.order_id == order_id)
-    results = db.session.execute(statement).scalars()
-    for product_order in results:
-        db.session.delete(product_order)
-    db.session.commit()
-    db.session.delete(order)
-    db.session.commit()
-    return redirect(url_for("html.orders"))
-
-
-@web_pages_bp.route("/orders/<int:order_id>/process", methods=["POST"])
-def process_order_web(order_id):
-    order = db.get_or_404(Order, order_id)
-    result = order.process_order()
-    db.session.commit()
-    print(result) #error if any
-    return redirect(url_for("html.orders"))
+@web_pages_bp.route("/teams")
+def teams():
+    return render_template("teams.html")
