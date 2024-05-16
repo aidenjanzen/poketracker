a8ccd7c563ff7e0c9eee8eaeeab490635bfa2571
Noah Thomson <noah.thomson777@gmail.com>
2024-05-16 13:30:21 -0700

    another test

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 from app import app
+from models import Users
 
 def test_connection():
     response = app.test_client().get('/')
     assert response.status_code == 200
+
+def test_user():
+    user = Users
+    user.username = "Noah Test"
+    user.password = "badpassword"
+    assert isinstance(user.username, str)
+    assert isinstance(user.password, str)
