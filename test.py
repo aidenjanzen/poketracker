c7fc76ff3f422f23886110af999cff84248a6f0c
aidenjanzen <aidenw.janzen@outlook.com>
2024-05-16 13:55:14 -0700

    more test code

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from app import app
 from models import Users
 from database import db
+import pytest
 
 def test_connection():
     response = app.test_client().get('/')
@@ -16,7 +17,28 @@ def test_user():
 def test_database():
     pass
 
-def test_index(client):
+@pytest.fixture
+def client():
+    return app.test_client()
+
+def test_base(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Hello, World!' in response.data
+    assert b'Home Page - nogen' in response.data
+
+def test_home(client):
+    response = client.get('/home')
+    assert response.status_code == 200
+    assert b'Home Page - nogen' in response.data
+
+def test_search(client):
+    response = client.get('/search')
+    assert response.status_code == 200
+    assert b'Search Page' in response.data
+
+def test_gen(client):
+    number = 1
+    response = client.get(f'/gen{number}')
+    assert response.status_code == 200
+    assert b'Generation 1' in response.data
+    assert b'1' in response.data  # Check if it includes start number
