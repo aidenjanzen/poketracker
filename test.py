a77383272a015faf87ec3f961ed1905d0e5cef51
aidenjanzen <aidenw.janzen@outlook.com>
2024-05-16 13:44:15 -0700

    exmaple tests

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from app import app
 from models import Users
+from database import db
 
 def test_connection():
     response = app.test_client().get('/')
@@ -11,3 +12,11 @@ def test_user():
     user.password = "badpassword"
     assert isinstance(user.username, str)
     assert isinstance(user.password, str)
+
+def test_database():
+    pass
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello, World!' in response.data
